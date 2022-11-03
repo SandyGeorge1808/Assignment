1c0cccb0af7dfa8b01908efc0e6be4f1c52c932a
SandyGeorge1808 <115519692+SandyGeorge1808@users.noreply.github.com>
2022-11-03 10:57:16 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,22 @@
+package org.college;
+
+public class College {
+	public void CollegeName() {
+		System.out.println("Kcg");
+	}
+	public void CollegeCode() {
+		System.out.println("3001");
+
+	}
+	public void CollegeRank() {
+		System.out.println("33");
+	}
+	public static void main(String[] args) {
+		College jj=new College();
+		jj.CollegeName();
+		jj.CollegeCode();
+		jj.CollegeRank();
+		System.out.println(jj);
+}
+}
+
---
+++ department/Department.java
@@ -0,0 +1,33 @@
+package org.department;
+import org.college.*;
+public class Department {
+	public static class department extends College {
+		public void DepartmentName() {
+			System.out.println("IT");
+		}
+		public static void main(String[] args) {
+			Department dept=new Department();
+			dept.CollegeCode();
+			dept.CollegeCode();
+			dept.CollegeRank();
+			dept.DepartmentName();
+			System.out.println(dept);
+		}
+
+	}
+
+	public void CollegeRank() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void CollegeCode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void DepartmentName() {
+		// TODO Auto-generated method stub
+		
+	}
+}
